8ddedb121923e17ab982f23a2638783fe5419948
nintend0ll <168145217+nintend0ll@users.noreply.github.com>
2024-05-15 11:46:14 -0300

    commit de inserts, tengo q seguir jeje

---
+++ INSERTUsuarios.sql
@@ -0,0 +1,14 @@
+﻿USE ObligatorioPapeleria2; 
+INSERT INTO [dbo].[Usuarios] (Rol_RolValor, Contrasenia_Password, ContraseniaEncriptada_ValorContrasenia, Email_DireccionEmail, NombreCompleto_Apellido, NombreCompleto_Nombre)  
+VALUES ('2', 'Ariana.1', 'Ariana.1', 'arianagrande@gmail.com', 'Grande', 'Ariana'),
+('1', 'Taylor.1', 'Taylor.1', 'taylorswift@gmail.com', 'Swift', 'Taylor'),
+('1', 'Lanita!1', 'Lanita!1', 'lanadelrey@gmail.com', 'Del Rey', 'Lana'),
+('1', 'XCXWorld1!', 'XCXWorld1!', 'charlixcx@gmail.com', 'XCX', 'Charli'),
+('1', 'underWater!1', 'underWater!1', 'chuu@gmail.com', 'Kim', 'JiWoo'),
+('1', 'Gowonnie!2', 'Gowonnie!2', 'gowon@gmail.com', 'Park', 'Chaewon'),
+('1', 'Twice2!', 'Twice2!', 'nayeon@gmail.com', 'Im', 'Nayeon'),
+('1', 'Momo.1', 'Momo.1!', 'momo@gmail.com', 'Hirai', 'Momo'),
+('1', 'Hyeju.1', 'Hyeju.1', 'hyeju@gmail.com', 'Son', 'Hyeju'),
+('1', 'Hyunjin.1', 'Hyunjin.1', 'hyunjin@gmail.com', 'Kim', 'Hyunjin');
+
+
