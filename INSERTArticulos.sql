465801e963a743ed5d92fcfab61ebcd51dd65056
nintend0ll <168145217+nintend0ll@users.noreply.github.com>
2024-05-15 12:36:47 -0300

    insert de articulos y clientes

--- INSERTArticulos.sql
+++ INSERTArticulos.sql
@@ -1,1 +1,12 @@
-﻿
+﻿USE ObligatorioPapeleria2; 
+INSERT INTO [dbo].[Articulos] (Nombre_NombreArticuloValor, Descripcion_DescripcionArticuloValor, Codigo_CodigoArticuloValor, PrecioVenta, Stock)
+VALUES ('Hojas A4', 'Hojas A4 para impresora', '8572019346852', 250, 150),
+('Lapices Faber Castell', 'Lapices Colores x24', '1234567891234', 150, 50),
+('Lapicera BIC', 'Lapicera BIC azul', '7932481674023', 99, 100),
+('Cuaderno rayado', 'Cuaderno rayado tapa dura', '5127398462058', 139, 100),
+('Sylvanian Family', 'Sylvanian Family blind box', '9273816405928', 459, 30),
+('Lápices HB', 'Lápices de grafito estándar', '4927610358274', 50, 200),
+('Marcadores de colores', 'Juego de marcadores de colores surtidos', '7518296403728', 80, 50),
+('Cinta adhesiva', 'Rollo de cinta adhesiva transparente', '3654928107365', 30, 300),
+('Calculadora básica', 'Calculadora de funciones básicas', '9246738102584', 200, 80),
+('Resaltadores fluorescentes', 'Juego de resaltadores de colores', '8367291047523', 120, 75);
--- INSERTClientes.sql
+++ INSERTClientes.sql
@@ -1,1 +1,12 @@
-﻿
+﻿USE ObligatorioPapeleria2; 
+INSERT INTO [dbo].[Clientes] (RazonSocial, Direccion_Calle, Direccion_Ciudad, Direccion_Distancia, Direccion_Numero, NombreCompleto_Apellido, NombreCompleto_Nombre, RUT_NroRut)
+VALUES ('Kawaii Gaming', 'Av Rivera', 'Montevideo', 5, 1234, 'Kim', 'Jiwoo', 195357789456),
+('Librería del Este', 'Av Italia', 'Montevideo', 10, 1567, 'Jo', 'Haseul', 205479630812),
+('Cube Gaming', 'Mataojo', 'Montevideo', 15, 1890, 'Jung', 'Jinsoul', 198765432109),
+('Imprenta Rápida', 'Comercio', 'Montevideo', 20, 1345, 'Jeon', 'Heejin', 203040506070),
+('Papel y Tinta', 'Calle 70', 'Canelones', 104, 1678, 'Kim', 'Hyunjin', 195678234567),
+('Tienda de Papel', 'Av Propios', 'Montevideo', 7, 1901, 'Im', 'Yeojin', 198765432156),
+('Papel y Lápiz', 'Mar de Ajo', 'Canelones', 106, 1237, 'Park', 'Chaewon', 205468732910),
+('La Papelería Creativa', 'Gestido', 'Canelones', 99, 1567, 'Wong', 'Gaahei', 198765432101),
+('Escritura', 'Rambla Costanera', 'Montevideo', 85, 1890, 'Ha', 'Sooyoung', 201980746352),
+('Arte en Papel', 'Av Loona', 'Montevideo', 120, 1123, 'Choi', 'Verim', 198765432102);
