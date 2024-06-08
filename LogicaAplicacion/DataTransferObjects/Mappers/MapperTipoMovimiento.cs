1501431c02748fffffa85dd8484cf2f7e8b91ce9
Sr-Lechuga <jonattanlima@gmail.com>
2024-06-08 18:37:12 -0300

    Update: Mapper tipo movimiento

---
+++ LogicaAplicacion/DataTransferObjects/Mappers/MapperTipoMovimiento.cs
@@ -0,0 +1,38 @@
+﻿using LogicaAplicacion.DataTransferObjects.Models.TipoMovimiento;
+using LogicaNegocio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaAplicacion.DataTransferObjects.Mappers
+{
+    public class MapperTipoMovimiento
+    {
+        public static TipoMovimiento FromDTO(TipoMovimientoDTO tipoMovimientoDTO)
+        {
+            return new TipoMovimiento
+            {
+                Id = tipoMovimientoDTO.Id,
+                Nombre = tipoMovimientoDTO.Nombre,
+                Reduccion = tipoMovimientoDTO.Reduccion
+            };
+        }
+
+        public static TipoMovimientoDTO ToDTO(TipoMovimiento tipoMovimiento)
+        {
+            return new TipoMovimientoDTO
+            {
+                Id = tipoMovimiento.Id,
+                Nombre = tipoMovimiento.Nombre,
+                Reduccion = tipoMovimiento.Reduccion
+            };
+        }
+
+        public static IEnumerable<TipoMovimientoDTO> FromList(IEnumerable<TipoMovimiento> tiposMovimiento)
+        {
+            return tiposMovimiento.Select(tipoMovimiento => ToDTO(tipoMovimiento));
+        }
+    }
+}
