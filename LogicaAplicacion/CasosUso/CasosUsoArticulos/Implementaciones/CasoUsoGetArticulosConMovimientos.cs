0e5951cb035e34f42d4c969c875b6ad2c8b0bb2d
Agustina Sánchez <168145217+nintend0ll@users.noreply.github.com>
2024-06-18 20:47:53 -0300

    tope push

--- LogicaAplicacion/CasosUso/CasosUsoArticulos/Implementaciones/CasoUsoGetArticulosConMovimientos.cs
+++ LogicaAplicacion/CasosUso/CasosUsoArticulos/Implementaciones/CasoUsoGetArticulosConMovimientos.cs
@@ -13,21 +13,21 @@ namespace LogicaAplicacion.CasosUso.CasosUsoArticulos.Implementaciones
 {
     public class CasoUsoGetArticulosConMovimientos : ICasoUsoGetArticulosConMovimientos
     {
-        private IRepositorioArticulos _repositorioArticulos { get; set; }
-        private IRepositorioSettings _settings { get; set; }
+        private IRepositorioArticulos RepositorioArticulos { get; init; }
+        private IRepositorioSettings RepositorioSettings { get; init; }
 
         public CasoUsoGetArticulosConMovimientos(
             IRepositorioArticulos repositorioArticulos, 
             IRepositorioSettings repositorioSettings)
         {
-            _repositorioArticulos = repositorioArticulos;
-            _settings = repositorioSettings;
+            RepositorioArticulos = repositorioArticulos;
+            RepositorioSettings = repositorioSettings;
         }
         
         public IEnumerable<ArticulosListadoDTO> GetArticulosConMovimientos(DateTime fecha1, DateTime fecha2, int pageNumber)
         {
-            int size = int.Parse(_settings.GetValueByName("PageSize") + "");
-            return _repositorioArticulos.GetArticulosConMovimientos(fecha1, fecha2, pageNumber, size)
+            int size = int.Parse(RepositorioSettings.GetValueByName("PageSize") + "");
+            return RepositorioArticulos.GetArticulosConMovimientos(fecha1, fecha2, pageNumber, size)
                                         .Select(a => MapperArticulo.ToDTO(a));
         }
     }
--- LogicaAplicacion/CasosUso/CasosUsoMovimientosDeStock/Implementaciones/CasoUsoAltaMovimientoStock.cs
+++ LogicaAplicacion/CasosUso/CasosUsoMovimientosDeStock/Implementaciones/CasoUsoAltaMovimientoStock.cs
@@ -13,18 +13,30 @@ namespace LogicaAplicacion.CasosUso.CasosUsoMovimientosDeStock.Implementaciones
     public class CasoUsoAltaMovimientoStock : ICasoUsoAltaMovimientoStock
     {
         public IRepositorioMovimientoDeStock RepositorioMovimientoStock { get; init; }
-
+        private IRepositorioSettings RepositorioSettings { get; init; }
         public CasoUsoAltaMovimientoStock(
-            IRepositorioMovimientoDeStock repositorioMovimientoDeStock
+            IRepositorioMovimientoDeStock repositorioMovimientoDeStock, 
+            IRepositorioSettings repositorioSettings
 
             )
         {
             // Inyeccion de dependencia
             RepositorioMovimientoStock = repositorioMovimientoDeStock;
+            RepositorioSettings = repositorioSettings;
         }
         public void AltaMovimientoStock(MovimientoDeStockDTO movimientosDeStockDTO)
         {
-            RepositorioMovimientoStock.Add(MapperMovimientoStock.FromDTO(movimientosDeStockDTO));
+            int tope = int.Parse(RepositorioSettings.GetValueByName("Tope") + "");
+            if (movimientosDeStockDTO.Cantidad <= tope)
+            {
+                RepositorioMovimientoStock.Add(MapperMovimientoStock.FromDTO(movimientosDeStockDTO));
+            }
+            else
+            {
+                throw new Exception("No se puede superar el tope " + tope);
+            }
+            
         }
+
     }
 }
